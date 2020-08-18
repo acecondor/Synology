6656cf18aaa9ba5698ef98064d298185c05e3d47
acecondor <57838961+acecondor@users.noreply.github.com>
2020-08-18 11:39:45 +0200

    Rilevo file modificati
    Prima bozza per riavvio servizio solo se i certificati sono cambiati da tot tempo

--- SyncthingCert/SyncthingCert.sh
+++ SyncthingCert/SyncthingCert.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 #
-# versione 2020.07.22.003
+# versione 2020.08.18.001
 #
 ##############################################################################
 #
 # allineamento certificato Synology > Synthing 
+
+# verifico se certificati origine sono cambiati nelle ultime 24 ore
+# diffday = differenza in giorni da verificare
+# cnt = numero fine cambiati nell'intervallo di data
+
+diff=1
+cnt=$(find /usr/syno/etc/certificate/system/default -mtime -$diff -ls | wc -l  2>&1)
+
+# differenza in minuti (alternativa)
+# cnt=$(find /usr/syno/etc/certificate/system/default -mmin -$diff -ls | wc -l  2>&1)
 
 # imposto percorso certificati Syncthing
 cert_orig="/usr/syno/etc/certificate/system/default"
@@ -29,3 +39,12 @@ chown sc-syncthing:syncthing $cert_dest/https-cert.pem
 chown sc-syncthing:syncthing $cert_dest/https-key.pem
 chmod 640 $cert_dest/https-cert.pem
 chmod 600 $cert_dest/https-key.pem
+
+# riavvia Syncthing solo se i file sono cambiati
+if [ cnt > 0 ]
+then
+  echo "Riavvia servizio... "$cnt
+  # inserire riavvio servizio, in costruzione
+else 
+  echo "Nulla da fare"
+fi
