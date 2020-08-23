d97e1b2aea64d39507280569250820af537ea6af
acecondor <57838961+acecondor@users.noreply.github.com>
2020-08-23 12:22:42 +0200

    Add comment and force option

--- SyncthingCert/SyncthingCert.sh
+++ SyncthingCert/SyncthingCert.sh
@@ -1,52 +1,57 @@
 #!/bin/bash
 #
-# versione 2020.08.19.002
+# versione 2020.08.23.001
 #
 ##############################################################################
 #
-# allineamento certificato Synology > Synthing 
-# eseguire come root
-# check status synoservicecfg --status pkgctl-syncthing
+# Use Synology default certificate for Syncthing web GUI
+# Eseguire come root / Run as root
+# Usare il parametro "force" per ignorare i controlli / Use parameters "force" to bypass check
 
-# verifico se certificati origine sono diversi da quelli di Syncthing (diff)
-
-# imposto percorso certificati Syncthing
+# imposto percorso certificati / set certificates path 
 cert_orig="/usr/syno/etc/certificate/system/default"
 cert_dest="/volume1/@appstore/syncthing/var"
 readonly cert_orig
 readonly cert_dest
 
-# verifico se certificati differenti
+# verifico se certificati differenti / check if certificates is different
 DIFF=$(diff $cert_orig/cert.pem $cert_dest/https-cert.pem)
+echo "Diff: $DIFF"
+if [ "$1" == "force" ]
+then
+  DIFF=="force"
+  echo "Forzo procedura / Force procedure"
+fi
 
 if [ "$DIFF" != "" ]
 then
-  echo "Eseguo aggiornamento certificati"
+  echo "Eseguo aggiornamento certificati / Run certificates update"
   
-  # elimino vecchi salvataggi
+  # elimino vecchi salvataggi / delete old backups
+  echo "Elimino vecchi salvataggi / Delete old backups"
   rm $cert_dest/https-cert.pem.bk
   rm $cert_dest/https-key.pem.bk
 
-  # creo nuovo salvataggio
+  # creo nuovo salvataggio / make new backups
+  echo "Creo nuovo salvataggio / Make new backups"
   mv $cert_dest/https-cert.pem $cert_dest/https-cert.pem.bk
   mv $cert_dest/https-key.pem $cert_dest/https-key.pem.bk 
 
-  # copio il corrente certificato predefinito
+  # copio il corrente certificato predefinito / copy new DSM cert to Syncthing
+  echo "Copio il corrente certificato predefinito / copy new DSM cert to Syncthing"
   cp $cert_orig/cert.pem $cert_dest/https-cert.pem
   cp $cert_orig/privkey.pem $cert_dest/https-key.pem
   
-  # imposto le autorizzazioni
-  echo "Imposto autorizzazioni file"
+  # imposto le autorizzazioni / set permissions
+  echo "Imposto autorizzazioni file / Set permissions"
   chown sc-syncthing:syncthing $cert_dest/https-cert.pem
   chown sc-syncthing:syncthing $cert_dest/https-key.pem
   chmod 640 $cert_dest/https-cert.pem
   chmod 600 $cert_dest/https-key.pem
   
-  # riavvio servizio Syncthing
-  echo "Riavvio servizio Syncthing"
+  # riavvio servizio Syncthing / restart Syncthing service
+  echo "Riavvio servizio Syncthing / Restart Syncthing service"
   synoservicecfg --restart pkgctl-syncthing
 else 
-  echo "Nulla da fare!"
+  echo "Nulla da fare! / Nothing to do!"
 fi
-
-
